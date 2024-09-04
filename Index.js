bf3597c0132b11fd23ac1b7e52608b8fbae99f05
subash <subash43it@proton.me>
2024-09-04 10:26:19 +0530

    Finding the small Number

--- Index.js
+++ Index.js
@@ -1,1 +1,11 @@
-console.log("Testing");
+let myVar = [7,34,53,4321];
+
+let minVal = myVar[0];
+
+for (let i = 0 ; i <= myVar.length; i++){
+    if (myVar[i] < minVal) {
+        minVal = myVar[i];
+    }
+}
+
+console.log(minVal)
