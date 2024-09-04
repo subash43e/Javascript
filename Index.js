56d8c905c333b7e214a06638200df6c491a4ef88
subash <subash43it@proton.me>
2024-09-04 11:32:30 +0530

    sorting array with buble sort algorithms

--- Index.js
+++ Index.js
@@ -1,11 +1,15 @@
-let myVar = [7,34,53,4321];
+const myArray = [64, 34, 25, 12, 22, 11, 90, 5];
+const n = myArray.length;
 
-let minVal = myVar[0];
 
-for (let i = 0 ; i <= myVar.length; i++){
-    if (myVar[i] < minVal) {
-        minVal = myVar[i];
+for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+        if (myArray[j] > myArray[j + 1]) {
+            const temp = myArray[j];
+            myArray[j] = myArray[j + 1];
+            myArray[j + 1] = temp;
+        }
     }
 }
 
-console.log(minVal)
+console.log("Sorted array:", myArray);
