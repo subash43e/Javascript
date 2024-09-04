bcf1f92031fbdb8b5d54c44a300c25746da7d8cb
subash <subash43it@proton.me>
2024-09-04 12:29:10 +0530

    improved version of bubble sort

--- Index.js
+++ Index.js
@@ -1,15 +1,19 @@
-const myArray = [64, 34, 25, 12, 22, 11, 90, 5];
+const myArray = [3, 7, 9, 12, 11];
 const n = myArray.length;
 
-
 for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-        if (myArray[j] > myArray[j + 1]) {
-            const temp = myArray[j];
-            myArray[j] = myArray[j + 1];
-            myArray[j + 1] = temp;
-        }
+  let swapped = false;
+  for (let j = 0; j < n - i - 1; j++) {
+    if (myArray[j] > myArray[j + 1]) {
+      const temp = myArray[j];
+      myArray[j] = myArray[j + 1];
+      myArray[j + 1] = temp;
+      swapped = true;
     }
+  }
+  if (!swapped) {
+    break;
+  }
 }
 
 console.log("Sorted array:", myArray);
